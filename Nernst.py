838ae4d4425e98fdd30d8caf5333671999071349
alanazulke <43655040+alanazulke@users.noreply.github.com>
2018-10-28 01:18:51 +0100

    Initial commit

---
+++ Nernst.py
@@ -0,0 +1,24 @@
+from numpy import log
+import matplotlib.pyplot as plt
+
+e0 = 2
+R = 8.34
+T = 298.15
+z = 1
+F = 96500
+output = list()
+x = list()
+
+for Q in range(0, 500, 1):
+    e = e0 - (R*T/(z*F))*log(Q)
+    print(e)
+    output.append(e)
+    x.append(log(Q))
+plt.plot(output, x)
+plt.ylabel('E/V')
+plt.show()
+
+
+
+
+
